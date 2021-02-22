92f4179899bf449603eb9d018c69b7a5bfe1b61f
Noore Alam Mahin <50703361+namahin@users.noreply.github.com>
2021-02-23 02:48:31 +0600

    responsible fixed

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,42 +1,48 @@
 // companies carousel
 $('.companies-carousel').owlCarousel({
-    loop:true,
-    margin:20,
-    nav:true,
-    responsive:{
-        0:{
-            items:2
+    loop: true,
+    margin: 20,
+    nav: false,
+    dots: false,
+    responsive: {
+        0: {
+            items: 2
         },
-        576:{
-            items:3
+        576: {
+            items: 3
         },
-        768:{
-            items:4
+        768: {
+            items: 4
         },
-        992:{
-            items:5
+        992: {
+            items: 5
         }
     }
 })
 
 // feedback carousel
 $('.feedback-carousel').owlCarousel({
-    loop:true,
-    margin:false,
-    nav:true,
+    loop: true,
+    margin: false,
+    nav: true,
     dots: false,
-    responsive:{
-        0:{
-            items:1
+    responsive: {
+        0: {
+            items: 1
         },
-        576:{
-            items:1
+        576: {
+            items: 1
         },
-        768:{
-            items:1
+        768: {
+            items: 1
         },
-        992:{
-            items:1
+        992: {
+            items: 1
         }
-    }
+    },
+    navContainer: ".feedback-nav",
+    navText: [
+        '<i class="far fa-long-arrow-left"></i>',
+        '<i class="far fa-long-arrow-right"></i>',
+    ],
 })
